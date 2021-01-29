797061193cf6e01c43c51ee9e78e88c3e6902724
Siddharth Patra <siddharthpatra77@gmail.com>
2021-01-29 15:47:32 +0530

    vaapas

--- landing_page/src/components/pages/Home.js
+++ landing_page/src/components/pages/Home.js
@@ -4,6 +4,7 @@ import '../../resources/css/home.css'
 import MountAbu from '../../resources/images/MountAbu.jpeg'
 import Fibre from '../../resources/images/fibre.png'
 import Matheran from '../../resources/images/matheran.jpg'
+import Foodscontainingfibre from '../../resources/images/foods.jpg'
 import Banner from '../body/banner'
 import Card from '../body/card'
 import Navbar from '../header/Navbar'
@@ -22,7 +23,7 @@ const About = () => {
                     <Card 
                     title="Mount Abu"
                     imageurl={MountAbu}
-                    author="sipun"
+                    author="Dimple Nagpal"
                     dateposted="January 20,2021"
                     category="Tour and Travels"
                     body="'The only hill station in the state of Rajasthan’"/>
@@ -41,12 +42,12 @@ const About = () => {
                     category="Tour and Travels"
                     body="Matheran is a beautiful town in Maharashtra that provides a peaceful holiday for a quick getaway. Here you will get to explore nature"/>
                     <Card 
-                    title="Mount Abu"
-                    imageurl={MountAbu}
-                    author="sipun"
-                    dateposted="January 20,2021"
-                    category="Tour and Travels"
-                    body="'The only hill station in the state of Rajasthan’"/>
+                    title="Importance of Fibre in your diet"
+                    imageurl={Foodscontainingfibre}
+                    author="aditya deshmukh"
+                    dateposted="January 11,2021"
+                    category="food and drinks|health and fitness"
+                    body="Do you consume fibre on a daily basis? A complete balanced diet includes 7 nutrients I.e.,"/>
                 </div>
                 <div className="bordertop"></div>
                 <div className="content">
