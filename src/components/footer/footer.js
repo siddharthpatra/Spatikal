212f0c89253c01ddb3465d12e3ef9f4f9da27e85
Siddharth Patra <siddharthpatra77@gmail.com>
2021-02-02 13:36:09 +0530

    footer changes

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -19,35 +19,27 @@ const Footer = () => {
                                 <li>Chattisgarh- 769850</li>
                             </ul>
                         </div>
+                    </div>
+                    <hr/>
+                    <div className="row displayFlex details">
                         <div className="column">
-                            <NavLink exact to ="/contact" className="nav-logo-footer">
-                            <h3> Contact</h3>
+                            <NavLink exact to ="/about" className="nav-logo-footer">
+                            <h3> About Us</h3>
                             </NavLink>
-                            <ul className="list-unstyled">
-                                <li>Direct</li>
-                                <li>Facebook</li>
-                                <li>Instagram</li>
-                            </ul>
                         </div>
                         <div className="column">
-                            <NavLink exact to="/about" className="nav-logo-footer">
-                                <h3>About Us</h3>
+                            <NavLink exact to="/contact" className="nav-logo-footer">
+                                <h3>Contact Us</h3>
                             </NavLink>
-                            <ul className="list-unstyled">
-                                <li>Shanti Nagar</li>
-                                <li>Bhilai</li>
-                                <li>Chattisgarh- 769850</li>
-                            </ul>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="row displayFlex social">
+                        <div className="column">
+                            <i className="fab fa-linkedin"></i>
                         </div>
                         <div className="column">
-                            <h3>
-                                Follow us at
-                            </h3>
-                            <ul className="list-unstyled">
-                                <li>Shanti Nagar</li>
-                                <li>Bhilai</li>
-                                <li>Chattisgarh- 769850</li>
-                            </ul>
+                            <i className="fab fa-instagram"></i>
                         </div>
                     </div>
                     <hr/>
