376d13d79bac1a2115bc8a150be2fb1e0b09c343
siddharthpatra <57652248+siddharthpatra@users.noreply.github.com>
2021-05-07 22:41:36 +0530

    Update about.js
    Changed

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,35 +9,44 @@ const About = () => {
             <p>ABOUT US</p>
             </div>
             <br></br>
-            <p>
                 <div className="aboutContent">
+        <p>
             Spatikal is an online platform where we aim to provide vast information  and insights on various topics. Our primary focus is on removing the digital garbage and bringing out a wider essence to the topic to which the audience can completely rely on. Spatikal is derived from a Sanskrit word 'SPHATIKA’ which means ‘crystal’. A  crystal is a representation of transparency and uniqueness which is what we aim on inculcating in our business model. We want to project transparency in the information & content we provide and uniqueness in the way we present it. 
-                </div>
-            </p>
+                
+        </p>
+        </div>
             <br></br>
-            <p>
+            
             <div className="aboutTitle">
+        <p>
             MISSION
+        </p>
             </div>
-            </p> 
+             
             <br></br>
-            <p>
+            
                 <div className="aboutContent">
+        <p>
             We aim at becoming a platform where all your research ends on a wide range of topics through written, video and audio content. 
+        </p> 
                 </div>
-            </p> 
+            
             <br></br>  
-            <p>
+            
             <div className="aboutTitle">
+        <p>
             VISION
+        </p>
             </div>
-            </p>
+            
             <br></br>
-            <p>
+           
                 <div className="aboutContent">
+        <p>
             Our vision is to become a successful social informative networking platform wherein people can come and voice out their opinions and views on various topics and build up a trust with our audience on the content they are viewing on our platform.
-                </div> 
-            </p>
+        </p>        
+        </div> 
+            
  
 
         </>
