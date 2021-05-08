b0481c4323c2670674ccf13d7bdd24b6300a6358
lakshya <lakshyasoni0811@gmail.com>
2021-05-08 19:02:29 +0530

    Testing 7.0

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,64 +1,67 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import '../../resources/css/footer.css'
+import "../../resources/css/footer.css";
 
 const Footer = () => {
-    return (
-        <>
-            <div className="footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="column">
-                            <NavLink exact to ="/" className="nav-logo-footer">
-                                <h2>Spatikal</h2>
-                            </NavLink>
-                            {/* <ul className="list-unstyled">
+  return (
+    <>
+      <div className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="column">
+              <NavLink exact to="/" className="nav-logo-footer">
+                <div className="footerLogo">
+                  <h2>Spatikal</h2>
+                </div>
+              </NavLink>
+              {/* <ul className="list-unstyled">
                                 <li>Shanti Nagar</li>
                                 <li>Bhilai</li>
                                 <li>Chattisgarh- 769850</li>
                             </ul> */}
-                        </div>
-                    </div>
-                   <hr/>
-                    <div className="row displayFlex details">
-                        <div className="column">
-                            <NavLink exact to ="/about" className="nav-logo-footer">
-                            <h3> About Us </h3>
-                            </NavLink>
-                        </div>
-                        <div className="column">
-                            <NavLink exact to="/contact" className="nav-logo-footer">
-                                <h3>Contact Us</h3>
-                            </NavLink>
-                        </div>
-                    </div>
-                    <hr/>
-                    <div className="row displayFlex social">
-                        {/* <div className="column">
+            </div>
+          </div>
+          <hr />
+          <div className="row displayFlex details">
+            <div className="column">
+              <NavLink exact to="/about" className="nav-logo-footer">
+                <h3> About Us </h3>
+              </NavLink>
+            </div>
+            <div className="column">
+              <NavLink exact to="/contact" className="nav-logo-footer">
+                <h3>Contact Us</h3>
+              </NavLink>
+            </div>
+          </div>
+          <hr />
+          <div className="row displayFlex social">
+            {/* <div className="column">
                             <i className="fab fa-linkedin"></i>
                         </div> */}
-                        <div className="column">
-                            <a href="https://www.instagram.com/_spatikal">
-                            <i className="fab fa-instagram"></i>
-                            </a>
-                        </div>
-                        <div className="column">
-                            <a href="https://www.facebook.com/Spatikal-104889145028341">
-                            <i className="fab fa-facebook"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <hr/>
-                    <div className="row terms">
-                        <p className="col-sm">
-                            &copy;{new Date().getFullYear()} Spatikal | All rights reserved | Terms of Service | Privacy
-                        </p>
-                    </div>
-                </div>
+            <div className="column">
+              <a href="https://www.instagram.com/_spatikal">
+                <i className="fab fa-instagram"></i>
+              </a>
             </div>
-        </>
-    )
-}
+            <div className="column">
+              <a href="https://www.facebook.com/Spatikal-104889145028341">
+                <i className="fab fa-facebook"></i>
+              </a>
+            </div>
+          </div>
+          <hr />
+          <div className="row terms">
+            <p className="col-sm">
+              &copy;{new Date().getFullYear()} Spatikal | All rights reserved |
+              Terms of Service | Privacy
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
