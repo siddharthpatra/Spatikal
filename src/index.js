9fe5ec537ba82c8c88a2474fbb7743b5c490fbbe
Siddharth Patra <siddharthpatra77@gmail.com>
2021-05-15 19:21:51 +0530

    Test SEO

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDom from "react-dom";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import '../src/resources/images/logo.png'
 
 const app = (
   <React.StrictMode>
