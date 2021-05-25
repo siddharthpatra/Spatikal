bb8e462ca06d65b9282b60a1af62f941d1bd88cd
lakshya <lakshyasoni0811@gmail.com>
2021-05-25 21:37:19 +0530

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-2T4D60PHLV"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+
 const app = (
   <React.StrictMode>
     <BrowserRouter>
