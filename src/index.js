52b8ba2418b6e8540776960d7239194ae7cdfab5
lakshya <lakshyasoni0811@gmail.com>
2021-05-27 20:36:18 +0530

    GA

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
 import ReactGA from 'react-ga';
-ReactGA.initialize('G-H78V6B9YBE'); // add your tracking id here.
+ReactGA.initialize('G-YG8SNCNTFH'); // add your tracking id here.
 ReactGA.pageview('/');
-
+ReactGA.pageview('/about');
+ReactGA.pageview('/blog');
+ReactGA.pageview('/contact');
 const app = (
   <React.StrictMode>
     <BrowserRouter>
